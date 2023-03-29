792b36230148c90ef11134634e72889d76664b16
Madhubala-06 <emadhubala1.@gmail.com>
2023-03-29 13:43:47 +0530

    feat :artist reviews in booking page

---
+++ main.js
@@ -0,0 +1,493 @@
+if (localStorage.getItem("card_data")) {
+  //   return;
+} else {
+  localStorage.setItem(
+    "card_data",
+    JSON.stringify([
+      {
+        image_url: "https://iili.io/HNYkZiv.jpg",
+        name: "Janani Makeup",
+        ruppi: "Rs 10,000",
+        localty: "Chennai",
+        makeupname: "Bridal Makeup",
+      },
+      {
+        image_url: "https://iili.io/HNY8RTX.jpg",
+        name: "subha makeup",
+        ruppi: "Rs 15,000",
+        localty: "kanyakumari",
+        makeupname: "Bridal Makeup",
+      },
+      {
+        image_url: "https://iili.io/HNYSRSV.jpg",
+        name: "Malikam makeup",
+        ruppi: "Rs 13,000",
+        localty: "Namakkal",
+        makeupname: "Bridal Makeup",
+      },
+      {
+        image_url: "https://iili.io/HNYUVLu.jpg",
+        name: "Wedding makeup",
+        ruppi: "Rs 20,000",
+        localty: "Trichy",
+        makeupname: "Bridal Makeup",
+      },
+      {
+        image_url: "https://iili.io/HNYgO6G.webp",
+        name: "Thulasi Makeup",
+        ruppi: "Rs 17,000",
+        localty: "chennai,Ambatur",
+        makeupname: "Bridal Makeup",
+      },
+      {
+        image_url: "https://iili.io/HNeBMx4.jpg",
+        name: "Madura Makeup",
+        ruppi: "13,000 Rs",
+        localty: "Dindukal",
+        makeupname: "Bridal Makeup",
+      },
+      {
+        image_url: "https://iili.io/HNeCsG2.jpg",
+        name: "Priya Makeup",
+        ruppi: "45,000 Rs",
+        localty: "Mumbai,Bhandup",
+        makeupname: "Bridal Makeup",
+      },
+      {
+        image_url: "https://iili.io/HNenOtj.webp",
+        name: "Queency Makeup",
+        ruppi: "22,000 Rs",
+        localty: "Bangolre",
+        makeupname: "Bridal Makeup",
+      },
+    ])
+  );
+}
+
+if (localStorage.getItem("artistreviews")) {
+  //   return;
+} else {
+  localStorage.setItem(
+    "artistreviews",
+    JSON.stringify([
+      [
+        {
+          input: "It was super experience",
+          comments: "The makeup artist was super",
+          artists_name: "Madura Makeup",
+        },
+        {
+          input: "Hi how are you?",
+          comments: "hi I like you❤❤❤❤❤",
+          artists_name: "Madura Makeup",
+        },
+        {
+          input: "Hi your dress is super.",
+          comments: "Can we have a catch up!",
+          artists_name: "Nadrftuio",
+        },
+        {
+          input: "It was super experience",
+          comments: "The makeup artist was super",
+          artists_name: "Nadrftuio",
+        },
+        {
+          input: "It was super experience",
+          comments: "The makeup artist was super",
+          artists_name: "Nadrftuio",
+        },
+        {
+          input: "Nice experince",
+          comments: "I liked you",
+          artists_name: "Madhu",
+        },
+        { input: "nice", comments: "nice", artists_name: "Nadrftuio" },
+        { input: "nice", comments: "nice", artists_name: "Madhu" },
+        {
+          input: "nice eperience",
+          comments: "Good",
+          artists_name: "Nadrftuio",
+        },
+        {
+          input: "nice eperience",
+          comments: "The makeup artist was super",
+          artists_name: "Nadrftuio",
+        },
+        { input: "", comments: "", artists_name: "Madura Makeup" },
+        {
+          input: "Nice experince",
+          comments: "The makeup artist was super",
+          artists_name: "Madura Makeup",
+        },
+        { input: "", comments: "", artists_name: "Madura Makeup" },
+        {
+          input: "It was super experience",
+          comments: "asd",
+          artists_name: "Madura Makeup",
+        },
+        {
+          input: "Nice experince",
+          comments: "The makeup artist was super",
+          artists_name: "Madura Makeup",
+        },
+        {
+          input: "Madhubala",
+          comments: "Nice experince",
+          artists_name: "Thulasi Makeup",
+        },
+        {
+          input: "It was super experience",
+          comments: "The makeup artist was super",
+          artists_name: "Wedding makeup",
+        },
+        {
+          input: "dfghjkl;",
+          comments: "cvbnm,",
+          artists_name: "Thulasi Makeup",
+        },
+        {
+          input: "Wonderful experince",
+          comments: "Nice experince",
+          artists_name: "Janani Makeup",
+        },
+        {
+          input: "Wonderful experince",
+          comments: "Good makeup",
+          artists_name: "Queency Makeup",
+        },
+        { input: "Good work", comments: "Nice", artists_name: "Priya Makeup" },
+        {
+          input: "It was super experience",
+          comments: "The makeup artist was super",
+          artists_name: "Priya Makeup",
+        },
+        {
+          input: "It was super experience",
+          comments: "The makeup artist was super",
+          artists_name: "Priya Makeup",
+        },
+        {
+          input: "It was super experience",
+          comments: "The makeup artist was super",
+          artists_name: "Priya Makeup",
+        },
+        {
+          input: "It was super experience",
+          comments: "The makeup artist was super",
+          artists_name: "Priya Makeup",
+        },
+        {
+          input: "It was super experience",
+          comments: "The makeup artist was super",
+          artists_name: "Priya Makeup",
+        },
+      ],
+    ])
+  );
+}
+
+if (localStorage.getItem("upload_details")) {
+  //   return;
+} else {
+  localStorage.setItem(
+    "upload_details",
+    JSON.stringify([
+      [
+        {
+          title: "SADFGHJKAETRYUIO",
+          textarea: "FGHJKL;",
+          uploadname: "Madhu",
+          imgurl: "https://iili.io/HXDBIh7.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "SADFGHJKAETRYUIO",
+          textarea: "FGHJKL;",
+          uploadname: " Nadrftuio",
+          imgurl: "https://iili.io/HXDBIh7.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "SADFGHJKAETRYUIO",
+          textarea: "FGHJKL;",
+          uploadname: "Nadrftuio",
+          imgurl: "https://iili.io/HXDBIh7.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "SADFGHJKAETRYUIO",
+          textarea: "FGHJKL;",
+          uploadname: "Nadrftuio",
+          imgurl: "https://iili.io/HXDBIh7.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "SADFGHJKAETRYUIO",
+          textarea: "FGHJKL;",
+          uploadname: "Nadrftuio",
+          imgurl: "https://iili.io/HXDBIh7.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "SADFGHJKAETRYUIO",
+          textarea: "FGHJKL;",
+          uploadname: "Madhu",
+          imgurl: "https://iili.io/HXDBIh7.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "SADFGHJKAETRYUIO",
+          textarea: "FGHJKL;",
+          uploadname: "Madhu",
+          imgurl: "https://iili.io/HXDBIh7.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "SADFGHJKAETRYUIO",
+          textarea: "FGHJKL;",
+          uploadname: "Madhu",
+          imgurl: "https://iili.io/HXDBIh7.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "SADFGHJKAETRYUIO",
+          textarea: "FGHJKL;",
+          uploadname: "Madhu",
+          imgurl: "https://iili.io/HXDBIh7.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "SADFGHJKAETRYUIO",
+          textarea: "FGHJKL;",
+          uploadname: "Madhu",
+          imgurl: "https://iili.io/HXDBIh7.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "SADFGHJKAETRYUIO",
+          textarea: "FGHJKL;",
+          uploadname: "Madhu",
+          imgurl: "https://iili.io/HXDBIh7.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "SADFGHJKAETRYUIO",
+          textarea: "FGHJKL;",
+          uploadname: "Madhu",
+          imgurl: "https://iili.io/HXDBIh7.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "SADFGHJKAETRYUIO",
+          textarea: "FGHJKL;",
+          uploadname: "Madhu",
+          imgurl: "https://iili.io/HXDBIh7.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "SADFGHJKAETRYUIO",
+          textarea: "FGHJKL;",
+          uploadname: "Nadrftuio",
+          imgurl: "https://iili.io/HXDBIh7.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "SADFGHJKAETRYUIO",
+          textarea: "FGHJKL;",
+          uploadname: "Nadrftuio",
+          imgurl: "https://iili.io/HXDBIh7.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "SADFGHJKAETRYUIO",
+          textarea: "FGHJKL;",
+          uploadname: "Nadrftuio",
+          imgurl: "https://iili.io/HXDBIh7.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "SADFGHJKAETRYUIO",
+          textarea: "FGHJKL;",
+          uploadname: "Nadrftuio",
+          imgurl: "https://iili.io/HXDBIh7.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "SADFGHJKAETRYUIO",
+          textarea: "FGHJKL;",
+          uploadname: "Nadrftuio",
+          imgurl: "https://iili.io/HXDBIh7.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "SADFGHJKAETRYUIO",
+          textarea: "FGHJKL;",
+          uploadname: "Nadrftuio",
+          imgurl: "https://iili.io/HXDBIh7.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "SADFGHJKAETRYUIO",
+          textarea: "FGHJKL;",
+          uploadname: "Nadrftuio",
+          imgurl: "https://iili.io/HXDBIh7.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "SADFGHJKAETRYUIO",
+          textarea: "FGHJKL;",
+          uploadname: "Nadrftuio",
+          imgurl: "https://iili.io/HXDBIh7.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "asdfghjkoiuytrewqertyui",
+          textarea: "dfghjkloiuytre",
+          uploadname: "dsfghjkuytrewqertyuio",
+          imgurl: "https://iili.io/HXGZAMP.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "asdfghjkoiuytrewqertyui",
+          textarea: "dfghjkloiuytre",
+          uploadname: "dsfghjkuytrewqertyuio",
+          imgurl: "https://iili.io/HXGZAMP.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "asdfghjkoiuytrewqertyui",
+          textarea: "dfghjkloiuytre",
+          uploadname: "dsfghjkuytrewqertyuio",
+          imgurl: "https://iili.io/HXGZAMP.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "asdfghjkoiuytrewqertyui",
+          textarea: "dfghjkloiuytre",
+          uploadname: "dsfghjkuytrewqertyuio",
+          imgurl: "https://iili.io/HXGZAMP.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "ghjklk,m",
+          textarea: "vcbnm,mn",
+          uploadname: "Madhu",
+          imgurl: "https://iili.io/HXGZAMP.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "ghjklk,m",
+          textarea: "vcbnm,mn",
+          uploadname: "Madhu",
+          imgurl: "https://iili.io/HXGZAMP.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "ghjklk,m",
+          textarea: "vcbnm,mn",
+          uploadname: "Madhu",
+          imgurl: "https://iili.io/HXGZAMP.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "ghjklk,m",
+          textarea: "vcbnm,mn",
+          uploadname: "Madhu",
+          imgurl: "https://iili.io/HXGZAMP.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "ghjklk,m",
+          textarea: "vcbnm,mn",
+          uploadname: "Madhu",
+          imgurl: "https://iili.io/HXGZAMP.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "vgbn",
+          textarea: "bk",
+          uploadname: "Madhu",
+          imgurl: "https://iili.io/HXGZAMP.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "hfgjkl;'",
+          textarea: "cvbnm,./",
+          uploadname: "Madhu",
+          imgurl: "https://iili.io/HXGZAMP.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "dfghjkl;'",
+          textarea: "dxfcgvhbjnkml,;.'/",
+          uploadname: "Madhu",
+          imgurl: "https://iili.io/HXpVZ9s.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "dfghjkl;'",
+          textarea: "dxfcgvhbjnkml,;.'/",
+          uploadname: "Madhu",
+          imgurl: "https://iili.io/HXpVZ9s.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "dfghjkl;'",
+          textarea: "dxfcgvhbjnkml,;.'/",
+          uploadname: "Madhu",
+          imgurl: "https://iili.io/HXpVZ9s.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "vbnm,./",
+          textarea: "vbjnkml,;",
+          uploadname: "Madura Makeup",
+          imgurl: "https://iili.io/HXGZAMP.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "GOod ",
+          textarea: "Nice",
+          uploadname: "Thulasi Makeup",
+          imgurl: "https://iili.io/HNYbfwJ.jpg",
+          mobilenumber: "91",
+        },
+        {
+          title: "dxfghjkl;",
+          textarea: "cvbnm,.",
+          uploadname: "Madura Makeup",
+          imgurl: "https://iili.io/HNexqhJ.jpg",
+          mobilenumber: "91",
+        },
+      ],
+    ])
+  );
+}
+
+if (localStorage.getItem("reviews")) {
+  //   return;
+} else {
+  localStorage.setItem(
+    "reviews",
+    JSON.stringify([
+      [
+        { name: "vbnm,./", customerReview: " nm,." },
+        { name: "xzcvbnm,.", customerReview: " fcgvhbjkl;" },
+        { name: "asd", customerReview: "sfdgr" },
+        { name: "sdfghj", customerReview: "cvbnm,." },
+        { name: "cvbnm", customerReview: "vbnm" },
+        { name: "sdfgh", customerReview: "dfgh" },
+        { name: "wedwrsty", customerReview: "qwserdstyu" },
+        { name: "asdfghjkl", customerReview: "sdfghjkl;" },
+        { name: "Madhubala", customerReview: "sdfghujiklo;" },
+        { name: "sxdcvfbnm,./", customerReview: "xfcgvjbnmk,." },
+        { name: "steffy", customerReview: "cvbnm,." },
+        { name: "prathiusha", customerReview: "it is very nice" },
+        { name: "sdfg", customerReview: "asdfghjkl;" },
+        { name: "sdfghjkl;", customerReview: "sdfghjkl;" },
+        { name: "asdfghjkl", customerReview: "rdtfygihjkl;" },
+      ],
+    ])
+  );
+}
+
