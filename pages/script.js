49fa316bbe1738d8279763ea55767d5efa0f63ee
Madhubala-06 <emadhubala1.@gmail.com>
2023-05-09 17:52:37 +0530

    fix:booking issues cleared

--- pages/script.js
+++ pages/script.js
@@ -7,7 +7,6 @@ let date = new Date();
 let currYear = date.getFullYear();
 let currMonth = date.getMonth();
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
 function renderCalendar() {
   const firstDayOfMonth = new Date(currYear, currMonth, 1).getDay();
   const lastDateOfMonth = new Date(currYear, currMonth + 1, 0).getDate();
@@ -19,9 +18,11 @@ function renderCalendar() {
     liTag += `<li class="inactive">${lastDateOfLastMonth - i + 1}</li>`;
   }
 
+  let selectedArtist=localStorage.getItem("artist_profile")
   for (let i = 1; i <= lastDateOfMonth; i++) {
     const isToday = i === date.getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear();
-    liTag += `<li class="${isToday ? "active" : ""}">${i}</li>`;
+    const isBooked = isDateBookedForArtist(i, currMonth, currYear, selectedArtist);
+    liTag += `<li class="${isToday ? "active" : ""} ${isBooked ? "booked" : ""}" ${isBooked ? "style='background-color: #ccc;'" : ""}>${i}</li>`;
   }
 
   for (let i = lastDayOfMonth; i < 6; i++) {
@@ -30,6 +31,19 @@ function renderCalendar() {
 
   currentDate.innerText = `${months[currMonth]} ${currYear}`;
   daysTag.innerHTML = liTag;
+}
+
+
+function isDateBookedForArtist(day, month, year, artistName) {
+  const records = JSON.parse(localStorage.getItem("records_booking_clients"));
+  if (!records) {
+    return false;
+  }
+  const filteredRecords = records.filter(record => {
+    const recordDate = new Date(record.event_date);
+    return recordDate.getDate() === day && recordDate.getMonth() === month && recordDate.getFullYear() === year && record.selected_artist_for_booking === artistName;
+  });
+  return filteredRecords.length > 0;
 }
 
 renderCalendar();
@@ -57,3 +71,25 @@ nextIcon.addEventListener("click", () => {
   }
   renderCalendar();
 });
+
+
+// const notifier = require('node-notifier');
+
+// // get the current date and time
+// const now = new Date();
+
+// // set the appointment date and time (replace with the actual appointment date and time)
+// const appointmentDate = new Date('2023-05-05T14:00:00');
+
+// // calculate the time difference between now and the appointment date in milliseconds
+// const timeDiff = appointmentDate.getTime() - now.getTime();
+
+// // set a timeout to show a reminder message to the user when it's time for the appointment
+// setTimeout(function() {
+//   notifier.notify({
+//     title: 'Appointment Reminder',
+//     message: 'Your appointment is in 1 hour!',
+//     sound: true, // play a notification sound
+//     wait: true, // wait for the user to dismiss the notification
+//   });
+// }, timeDiff - (60 * 60 * 1000)); // subtract 1 hour from the time difference to show the message 1 hour before the appointment
